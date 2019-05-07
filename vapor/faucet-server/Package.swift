0ded8b694e0eaf09405f687230d204246e44da55
cezres <cezr@sina.com>
2019-05-07 20:33:29 +0800

    Update Package.swift

--- vapor/faucet-server/Package.swift
+++ vapor/faucet-server/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "faucet-server",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/cezres/SQLite.swift.git", from: "0.11.6"),
+        .package(url: "https://github.com/cezres/SQLite.swift.git", from: "0.11.7"),
         .package(url: "https://github.com/cezres/ckb-sdk-swift", .revision("218b1d6bc77aae5679a3ebf88a780d175e03aeac")),
     ],
     targets: [
